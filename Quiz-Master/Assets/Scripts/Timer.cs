a6d85a4392e7331104d0366d630a96d38e49e24e
Gokay Iseri <gokay.iseri@outlook.com>
2024-06-24 20:45:48 +0300

    Timer Created

---
+++ Quiz-Master/Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+	[SerializeField] float timeToCompleteQuestion = 10f;
+	[SerializeField] float timeToShowCorrectAnswer = 3f;
+	float timerValue;
+
+	public bool isAnsweringQuestion = false;
+
+	void Update()
+	{
+		UpdateTimer();
+	}
+
+	void UpdateTimer()
+	{
+		timerValue -= Time.deltaTime;
+		if (isAnsweringQuestion)
+		{
+			if (timerValue <= 0)
+			{
+				isAnsweringQuestion = false;
+				timerValue = timeToShowCorrectAnswer;
+			}
+		}
+		else
+		{
+			if (timerValue <= 0)
+			{
+				isAnsweringQuestion = true;
+				timerValue = timeToCompleteQuestion;
+			}
+		}
+		Debug.Log(timerValue);
+	}
+}
