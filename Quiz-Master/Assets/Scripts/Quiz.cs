242a17b53b005e59bb8654e466038fa1e3e25534
Gokay Iseri <gokay.iseri@outlook.com>
2024-06-24 19:05:24 +0300

    OnAnswerSelected Method Added

--- Quiz-Master/Assets/Scripts/Quiz.cs
+++ Quiz-Master/Assets/Scripts/Quiz.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class Quiz : MonoBehaviour
     QuestionSO question;
     [SerializeField]
     GameObject[] answerButtons;
+    [SerializeField]
+    Sprite defaultButtonSprite;
+    [SerializeField]
+    Sprite correctAnswerButtonSprite;
+    int correctAnswerIndex;
 
     void Start()
     {
@@ -24,6 +30,25 @@ public class Quiz : MonoBehaviour
         for (int i = 0; i < answerButtons.Length; i++)
         {
             answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerButtonSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Correct answer was:\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerButtonSprite;
         }
     }
 }
